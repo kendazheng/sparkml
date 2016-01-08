520e2c1969d33dfd385f6b86f62d05b599b1df57
zhenglei <zhenglei@shinezone.com>
2016-01-08 13:42:10 +0800

    调用spark的决策树算法实现

---
+++ decision_tree.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+from numpy import array
+from pyspark import SparkContext
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.tree import DecisionTree
+
+sc = SparkContext()
+
+result = {1.0: 'yes', 0.0: 'no'}
+data = [
+    LabeledPoint(1, [1, 1]),
+    LabeledPoint(1, [1, 1]),
+    LabeledPoint(0, [1, 0]),
+    LabeledPoint(0, [0, 1]),
+    LabeledPoint(0, [0, 1])
+]
+
+rdd = sc.parallelize(data)
+
+print '------------------------------------'
+print type(rdd), dir(rdd)
+print rdd.collect()
+print '------------------------------------'
+model = DecisionTree.trainClassifier(rdd, 3, {})
+
+# print(model)
+
+print '********************************************************'
+print(model.toDebugString())
+print "test [1,0]: %s" % (result[model.predict(array([1, 0]))])
+print "test [1,1]: %s" % (result[model.predict(array([1, 1]))])
+print "test [0,0]: %s" % (result[model.predict(array([0, 0]))])
+print '********************************************************'
+sc.stop()
