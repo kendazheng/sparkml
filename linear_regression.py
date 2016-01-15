fd312d0d3963580fc9022b6c28d1abe8646f257a
zhenglei <zhenglei@shinezone.com>
2016-01-15 17:15:03 +0800

    调节线性回归参数

--- linear_regression.py
+++ linear_regression.py
@@ -4,7 +4,7 @@ Program: LinearRegressionWithSGD
 Description: 调用spark内置的线性回归算法示例
 Author: zhenglei - zhenglei@shinezone.com
 Date: 2016-01-14 13:40:31
-Last modified: 2016-01-15 16:11:43
+Last modified: 2016-01-15 17:07:38
 Python release: 2.7
 """
 # 调用Spark内置的LinearRegression算法对机器学习实战中的第八章鲍鱼年纪的预测
@@ -24,8 +24,11 @@ def textParser():
 if __name__ == '__main__':
     sc = SparkContext()
     datas = sc.parallelize(textParser())
-    model = LinearRegressionWithSGD.train(datas, step=2, iterations=100)
-    # print model.toDebugString()
+    model = LinearRegressionWithSGD.train(datas, step=2, iterations=100, intercept=True, regType='l2')
+    print '**' * 50
+    print model.weights
+    print model.intercept
+    print '**' * 50
     # 计算预测模型与训练值得方差
     prevals = datas.map(lambda p: (p.label, model.predict(p.features)))
     MSE = prevals.map(lambda (v, p): (v - p) **
