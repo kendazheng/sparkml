bc1ac755b0acf16462de53dd744fac98cc2a0383
zhenglei <zhenglei@shinezone.com>
2016-01-18 13:22:05 +0800

    添加执行文件

---
+++ excute.sh
@@ -0,0 +1,14 @@
+#!/bin/shell
+python decision_tree.py
+python logistic_regression.py
+python naive_bayes.py
+python svm_horse.py svm_digits.py
+python random_decision_tree.py
+python gbts_decision_tree.py
+python linear_regression.py
+python kmeans.py
+python gmm.py
+python fp_growth.py
+python pca.py
+python als.py
+python cardPredict.py
