5247ea805e0c01469045d0884ee0ad468e01c0f7
zhenglei <zhenglei@shinezone.com>
2016-02-14 09:39:48 +0800

    bug

--- pca.py
+++ pca.py
@@ -4,7 +4,7 @@ Program: PCA
 Description: 调用spark内置的PCA算法
 Author: zhenglei - zhenglei@shinezone.com
 Date: 2016-01-14 13:45:02
-Last modified: 2016-01-14 13:45:42
+# Last modified: 2016-01-28 19:23:14
 Python release: 2.7
 """
 # 调用spark内置的pca算法对机器学习实战中的第十三章数据集进行降维处理
