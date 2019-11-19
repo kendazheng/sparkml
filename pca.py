015def43822c506a85f10d49d09bc84d7dad6207
kendazheng <kendazheng@163.com>
2019-11-19 19:37:49 +0800

    Update pca.py

--- pca.py
+++ pca.py
@@ -2,7 +2,7 @@
 """"
 Program: PCA
 Description: 调用spark内置的PCA算法
-Author: zhenglei - zhenglei@shinezone.com
+Author: zhenglei - kendazheng@163.com
 Date: 2016-01-14 13:45:02
 # Last modified: 2016-01-28 19:23:14
 Python release: 2.7
